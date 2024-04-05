3b147b4b105b9ea3806d29e2971091cfcb1412b9
Kethytektek <163605882+Kethytektek@users.noreply.github.com>
2024-04-05 15:53:25 -0300

    doryzinha

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Programa Tradutor Baleês");
+Console.WriteLine();
+
+Console.WriteLine("Digite um texto a ser traduzido para Baleês:");
+Console.WriteLine();
+
+String textobalees = Console.ReadLine()!;
+
+Console.WriteLine("\nTraduzindo para baleês...\n");
+
+Console.WriteLine(textobalees
+.Replace("a" , "aaa")
+.Replace("e" , "eee")
+.Replace("i" , "iii")
+.Replace("o" , "ooo")
+.Replace("u" , "uuu")
+);
